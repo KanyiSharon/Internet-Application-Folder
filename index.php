0eaaa373de14482ead3fee52a970e4bcde3d95c4
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-29 11:09:54 +0300

    made changes

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>This is my first page</h1>
+    <?php
+require_once "load.php";
+print $Obj->user_age("Alex", 2004);
+?>
+</body>
+
+</html>
